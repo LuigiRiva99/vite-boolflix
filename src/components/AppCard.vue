5329d786c3204b8d20bb76520098994c2105ac58
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-04-16 16:05:18 +0200

    ADD: card images

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -7,14 +7,21 @@
             <img v-if="element.original_language === 'en'" width="30" height="30" src="https://img.icons8.com/emoji/48/us-outlying-islands-emoji.png" alt=""/>
             <img v-if="element.original_language === 'ja'" width="30" height="30" src="https://img.icons8.com/color/48/japan.png" alt="japan"/>
         </p>
-        <p>Voto: {{ element.vote_average }}</p>
+        <p>Voto: {{ starConverter(element.vote_average) }}</p>
+        <img :src="`https://image.tmdb.org/t/p/w154/${element.poster_path}`" alt="">
 </template>
 
 <script>
-import { store } from '../store.js'
 
 export default {
-    props : ['element','title','originalTitle']
+    props : ['element','title','originalTitle'],
+
+    methods: {
+        starConverter() {
+            const score =  Math.floor(vote / 2)
+            return score
+        }
+    }
 }
 </script>
 
